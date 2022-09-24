574ae0988db2f35f989e6ac4240fa5a9cad80434
HermanoCastro65 <hermanocastro18@gmail.com>
2022-09-23 22:01:16 -0300

    create methods getJogada and remove unused vars

--- src/jogo/Jogada.java
+++ src/jogo/Jogada.java
@@ -9,23 +9,8 @@ package jogo;
 @SuppressWarnings("resource")
 public class Jogada {
 
-	/**
-	 * Ambos os atriutos fazem feferência ao eixo x-y na matriz que representam
-	 * o tabuleiro.
-	 */
-	private int x;
-	private int y;
-
 	public Jogada(String str) throws JogadaInvalidaException {
-		parseString(str);
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public int getY() {
-		return y;
+		getJogada(str);
 	}
 
 	/**
@@ -35,15 +20,9 @@ public class Jogada {
 	 * @param jogada
 	 * @throws JogadaInvalidaException
 	 */
-	private void parseString(String jogada) throws JogadaInvalidaException {
+	private String getJogada(String jogada) throws JogadaInvalidaException {
 		try {
-			// Separam a string em tokens delimitados a partir de ",", "." ou " ".
-			String[] tokens = jogada.split("[\\,]|[\\.]|[\\s]");
-
-			// Armazeam os toquens e elimina eventuais espaços residuais.
-			this.x = Integer.parseInt(tokens[0].trim());
-			this.y = Integer.parseInt(tokens[1].trim());
-
+			return jogada;
 		} catch (Exception e) {
 			throw new JogadaInvalidaException("Jogada inválida!");
 		}
